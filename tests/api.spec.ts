5e220e04f6092b05b7bb92302d35101f5caeb0bc
Nazrin Asari <nazrin.asari@tranglo.com>
2026-02-10 16:49:20 +0800

    add serial test

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -5,25 +5,60 @@ import { postRequestAuth } from '../helpers/request-helper/auth'
 import { validateSuccessAuth, validateBadRequest } from '../helpers/response-helper/auth'
 
 
-const url = config.baseUrl;
+// const url = config.baseUrl;
 
-test('verify success response when auth with valid credentials', async ({ request }) => {
-    const response = await postRequestAuth(url, request, testData.validCredentials)
-    await validateSuccessAuth(response);
-});
+// test('verify success response when auth with valid credentials', async ({ request }) => {
+//     const response = await postRequestAuth(url, request, testData.validCredentials)
+//     await validateSuccessAuth(response);
+// });
 
-test('verify success response when auth with wrong username', async ({ request }) => {
-    const response = await postRequestAuth(url, request, testData.wrongUsername)
-    await validateBadRequest(response);
-});
+// test('verify success response when auth with wrong username', async ({ request }) => {
+//     const response = await postRequestAuth(url, request, testData.wrongUsername)
+//     await validateBadRequest(response);
+// });
 
-test('verify success response when auth with wrong password', async ({ request }) => {
-    const response = await postRequestAuth(url, request, testData.wrongPassword)
-    await validateBadRequest(response);
-});
+// test('verify success response when auth with wrong password', async ({ request }) => {
+//     const response = await postRequestAuth(url, request, testData.wrongPassword)
+//     await validateBadRequest(response);
+// });
 
 
-test('verify success response when auth with empty credentials', async ({ request }) => {
-    const response = await postRequestAuth(url, request, testData.emptyCredentials)
-    await validateBadRequest(response);
+// test('verify success response when auth with empty credentials', async ({ request }) => {
+//     const response = await postRequestAuth(url, request, testData.emptyCredentials)
+//     await validateBadRequest(response);
+// });
+
+test.describe('All requests', async () => {
+    let token: string = '';
+    test.describe.configure({ mode: 'serial' })
+    test('Auth', async ({ request }) => {
+        const response = await request.post('token-url-here', {
+            form:
+            {
+                //auth here
+
+            },
+            ignoreHTTPSErrors: true
+        })
+
+
+
+        const responseBody = await response.json();
+        token = responseBody.access_token;
+    });
+
+    test('DoPayment', async ({ request }) => {
+        const response = await request.post('payment API here', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            },
+            data:
+            {
+                //    sample data here
+            },
+            ignoreHTTPSErrors: true
+        })
+
+        console.log(await response.json());
+    });
 });
